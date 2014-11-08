c7de4228bc0fa4fe2fe68cd02b27a949c77448d5
Janos Szurovecz <szjani@szjani.hu>
2014-11-08 19:33:03 +0100

    doctrine CLI tool can be run from the root

--- cli-config.php
+++ cli-config.php
@@ -4,7 +4,7 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Helper\HelperSet;
 
-$container = require_once __DIR__ . '/../base.php';
+$container = require_once __DIR__ . '/base.php';
 
 /* @var $em Doctrine\ORM\EntityManager */
 $em = EntityManager::create($app['doctrine.connection'], $app['doctrine.config'], $app['doctrine.eventmanager']);
@@ -13,6 +13,6 @@ return new HelperSet(
     array(
         'db' => new ConnectionHelper($em->getConnection()),
         'em' => new EntityManagerHelper($em),
-        'dialog' => new \Symfony\Component\Console\Helper\DialogHelper()
+        'dialog' => new \Symfony\Component\Console\Helper\QuestionHelper()
     )
 );
